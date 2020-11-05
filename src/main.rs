d9b47dd81b5fca7dafb3258160da93591654a800
Caio Tavares <caio.tavares11@gmail.com>
2020-11-05 17:49:56 -0300

    Replicate C hello-world behaviour

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
 use msp432p401r as pac;
 use panic_halt as _;
 use msp432p401r::{WDT_A, DIO, Peripherals};
 
 fn stop_watchdog_timer(peripherals: &Peripherals) {
-    hprintln!("Stopping watchdog timer").unwrap();
     peripherals.WDT_A.wdtctl.modify(|r, w| unsafe {
         let watchdog_password: u16 = 0x5A00;
         let hold: u16 = (r.bits() | 0x0080) & 0x00FF; // Set bit 7 to one, everything else stays the same
@@ -16,31 +14,31 @@ fn stop_watchdog_timer(peripherals: &Peripherals) {
     });
 }
 
-fn set_output_dir(peripherals: &Peripherals) {
-    hprintln!("Setting output dir").unwrap();
+fn set_p1_0_output_dir(peripherals: &Peripherals) {
     peripherals.DIO.padir.modify(|r, w| unsafe {
         w.p1dir().bits(r.p1dir().bits() | 0x01)
     });
 }
 
+fn toggle_p1_0(peripherals: &Peripherals) {
+    peripherals.DIO.paout.modify(|r, w| unsafe {
+        w.p1out().bits(r.p1out().bits() ^ 0x01)
+    });
+}
+
 #[entry]
 fn main() -> ! {
-    hprintln!("Program start").unwrap();
-
     let peripherals = pac::Peripherals::take().unwrap();
 
     stop_watchdog_timer(&peripherals);
-    set_output_dir(&peripherals);
+    set_p1_0_output_dir(&peripherals);
 
     loop {
-        hprintln!("Loop start").unwrap();
+        toggle_p1_0(&peripherals);
 
-        peripherals.DIO.paout.modify(|r, w| unsafe { w.p1out().bits(r.p1out().bits() | 0x01) });
-        let mut delay = 50000;
+        let mut delay = 100000;
         while delay > 0 {
             delay = delay - 1;
         }
-        hprintln!("Loop end").unwrap();
-        peripherals.DIO.paout.modify(|r, w| unsafe { w.p1out().bits(r.p1out().bits() & 0x00) });
     }
 }
